924d287e13462519446840b5d3b00fc50c11d846
qa-bburi-5-rled@example.com <qa-bburi-5-rled@example.com>
2023-05-10 15:43:57 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16434.stoplight-dev.com/api/v1/projects/cHJqOjUzMg/spectral.js?branch_slug=main&token=810a462b-8390-4776-a0e5-ce1e0cdc7872";
+export default { extends: ruleset };
